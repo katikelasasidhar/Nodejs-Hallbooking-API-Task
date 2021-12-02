16af508fd9b69353c035f7904e07b4b35cd8f96a
Sam Smith <sasidhar.katikela@gmail.com>
2021-12-02 21:03:05 +0530

    new changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=express();
 const dotenv=require('dotenv');
 dotenv.config();
 app.use(express.json());
-const PORT=process.env.PORT;
+const PORT=process.env.PORT||5010;
 let halls=[{
     name:"normal hall",
     seats:70,
